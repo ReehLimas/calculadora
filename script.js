d00849a2f0975a62dba81295b4e73e5c67c1c187
renata.lima <reehlimasil08@gmail.com>
2023-12-06 16:58:21 -0300

    ok

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function calculate(){
 document.addEventListener('keydown', function(event){
     const verifica = /^[0-9]$/;
     if(verifica.test(event.key)){
-        valor(event)
+        valor(event.key)
 }
     if((event.key == 'Enter') ){
         calculate();
